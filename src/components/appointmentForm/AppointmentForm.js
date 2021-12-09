db38d15d9c924812d55052895840d890037037e3
barrettm51 <32210357+barrettm51@users.noreply.github.com>
2021-12-08 16:18:56 -0800

    add date selector and time selector to appointment form

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -27,9 +27,9 @@ export const AppointmentForm = ({
       <label>Title</label>
       <input name='title' type='text' onChange={handleChange} value={title} />
       <label>Date</label>
-      <input name='date' type='text' onChange={handleChange} value={date} />
+      <input name='date' type='date' min={getTodayString()} onChange={handleChange} value={date} />
       <label>Time</label>
-      <input name='time' type='text' onChange={handleChange} value={time} />
+      <input name='time' type='time' onChange={handleChange} value={time} />
       <ContactPicker contacts={contacts}
                      handleChange={handleChange}
                      appointmentContact={appointmentContact} />
